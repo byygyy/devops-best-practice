f4d052618378886389f66926390cf26bd7c0aa68
robin <lhh_nj@163.com>
2022-07-02 23:28:05 +0800

    add new about jenkins

---
+++ jenkins/java-install.groovy
@@ -0,0 +1,16 @@
+node("cm-linux-ansible") {
+      properties([
+          parameters([
+            string(name:'NODENAME', defaultValue:'cm-linux-ansible', description:'input the node name'),
+            string(name:'TARGET', defaultValue:'127.0.0.1', description:'input the server fqdn')
+      ])
+    ])
+
+    stage('Build'){
+      echo 'I am start to do the build'
+      git 'https://github.com/byygyy/platform-ansible.git'
+      sh '''
+         ansible-playbook -i 'gcp.lhh.pub,' java-install.yml
+         '''
+    }
+}
